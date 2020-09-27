399b1d959b46da64ab01a0080d433e3b4f060b92
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2020-09-27 15:51:14 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import struct
 import glob
 from fernet_custom import *
 from hash_lib import Hash
-
+from tkinter.messagebox import *
+from tkinter import *
 
 
 
@@ -180,8 +181,8 @@ def log(status):
             elif todo==False:
                 allow=False
                 logfile.write('[+] Starting Database \n')
-                while allow==False:
-                    database_pass=input("Database Password (press q to quit) :")
+                if allow==False:
+                    database_pass=password_login.get()
                     if database_pass=="q":
                         with open('var.txt','w') as varfile:
                             varfile.write('out')
@@ -189,7 +190,7 @@ def log(status):
                     hash2=Hash(database_pass)
                     allow=hash2.verify()
                     if allow==False:
-                        print("Wrong Password")
+                        messagebox.showwarning("Warning","Incorrect Password")
                         logfile.write(f"[_] Connexion Attempt with Password :{database_pass}")
                     else:
                         print("Login Successfully")
@@ -269,9 +270,9 @@ def gestionnaire():
     conn.close()
 
 def add_mdp():
-    user=input("User :")
-    password=input("Password :")
-    site=input("Site :")
+    user=useradd.get()
+    password=userpass.get()
+    site=usersite.get()
     data=(user,password,site)
     conn = sqlite3.connect('gestionnaire.db')
     cur = conn.cursor()
@@ -284,32 +285,44 @@ def add_mdp():
     conn.close()
 
 
+
 with open('var.txt','w') as varfile:
         varfile.write('out')
 
-
-while True:
+def verify():
     with open('var.txt','r') as varfile:
         data=varfile.read()
         if data=="in":
             loginstatus='Logged'
         else:
             loginstatus='Not Logged'
-    choix=int(input(f"[-{loginstatus}-] - 1.Flush 2.Create/Change Pass (if logged) 3.Login 4.Add 5.Content 6.Logout   :"))
+
+
+    text="Status: "+str(loginstatus)
+    l6=LabelFrame(root,text="LOGIN STATUS")
+    l6.pack()
+    textbox3=Label(l6,text=text).pack(side="left")
+    l6.place(relx=0.5, relheight=0.12, relwidth=0.5,rely=0.55)
+    
+    root.after(3000,verify)
+
+def choix(choix):
+    #choix=int(input(f"[--] - 1.Flush 2.Create/Change Pass (if logged) 3.Login 4.Add 5.Content 6.Logout   :"))
     if choix==1:
         flush()
     elif choix==2:
+        choix_2=check_var.get()
+        password_user=password_create.get()
         if 'gestionnaire_encrypted.db' in glob.glob("*.db"):
-            choix_2=input('You will Destroy the Current DB. Continue ? y/n :')
-            if choix_2=="y":
-                os.remove('gestionnaire_encrypted.db')
+            if choix_2=="1":
+                flush()
             else:
                 with open('var.txt','w') as varfile:
                     varfile.write('out')
                 sys.exit()
         create()
         gestionnaire()
-        password_user=input('Your password :')
+        
 
         hash_user=Hash(password_user)
         with open('accounthash.hash','w') as hashwrite:
@@ -336,3 +349,109 @@ while True:
         decrypt('password')            # DEV TOOLS
     #################################### DEV TOOLS
     print('\n')
+
+def refresh():
+    if 'gestionnaire.db' in glob.glob("*.db"):
+            conn = sqlite3.connect('gestionnaire.db')
+    else:
+        print('Connecting To Encrypted Database')
+        conn = sqlite3.connect('gestionnaire_encrypted.db')
+    cur = conn.cursor()
+    cmd=f"SELECT * FROM gestionnaire;"
+    cur.execute(cmd)
+    conn.commit()
+
+    retour = cur.fetchall()
+    for elem_remove in retour:
+        listNodes.delete("0")
+    for elem in retour:
+        liste_mots=[]
+        for element in elem:
+            liste_mots.append(element)
+        try:
+            if liste_mots[0]!='0' and liste_mots[1]!='0' and liste_mots[2]!='Values_Init':
+                text=f"  User: {liste_mots[0]}   Password: {liste_mots[1]}   site : {liste_mots[2]}"
+                listNodes.insert(END,text)
+        except:
+            pass
+
+def remove():
+    pass
+
+def start_server():
+    print('starting server')
+
+root = Tk()
+root.title("PasswordKeeper")
+root.geometry("550x550")
+password_create=StringVar()
+check_var=IntVar()
+password_login=StringVar()
+useradd=StringVar()
+userpass=StringVar()
+usersite=StringVar()
+
+l=LabelFrame(root, text="LOG OPTIONS")
+l.pack(side=RIGHT)
+
+l2=LabelFrame(root,text='ACTIONS')
+l2.pack(side=LEFT)
+
+l3=LabelFrame(root,text="CONTENT")
+l3.pack()
+
+l4=LabelFrame(root,text="ADD")
+l4.pack()
+
+l5=LabelFrame(root,text="SERVER")
+l5.pack()
+
+l6=LabelFrame(root,text="LOGIN STATUS")
+l6.pack()
+
+flushing = Button(l2, text='Flush',command=lambda: choix(1)).pack()
+
+
+text=Label(l2,text="Password For New db :").pack()
+password_creation= Entry(l2, textvariable=password_create).pack() 
+creation = Button(l2, text='Create Or Change Password',command=lambda: choix(2)).pack()
+check=Checkbutton(l2, text="Destroy previous", variable=check_var).pack()
+
+text2=Label(l,text="Database Password (q to quit):").pack()
+password_entry = Entry(l, textvariable=password_login).pack() 
+submit = Button(l, text='Connect',command=lambda: choix(3)).pack(side=LEFT)
+submit2 = Button(l, text='Disconnect',command=lambda:choix(6)).pack(side=RIGHT)
+
+text3=Label(l4,text="User :").pack()
+User_Entry = Entry(l4, textvariable=useradd).pack() 
+text3=Label(l4,text="Password :").pack()
+Password_User_Entry = Entry(l4, textvariable=userpass).pack() 
+text4=Label(l4,text="Site :").pack()
+site_user_entry = Entry(l4, textvariable=usersite).pack()
+submit21 = Button(l4, text='Add',command=lambda: choix(4)).pack()
+
+serverbutton=Button(l5,text='Start Server', command=start_server).pack()
+
+listNodes=Listbox(l3, width=75, heigh=10)
+listNodes.pack(side=RIGHT)
+
+scrollbar=Scrollbar(l3, orient="vertical")
+scrollbar.config(command=listNodes.yview)
+scrollbar.pack(side="right", fill='y')
+
+listNodes.config(yscrollcommand=scrollbar.set)
+
+refresh_button=Button(l3,text="Refresh",command=refresh).pack()
+remove_button=Button(l3,text="Remove", command=remove).pack()
+
+
+l.place(relx=0, relheight=0.25, relwidth=0.5)
+l2.place(relx=0.5, relheight=0.55, relwidth=1.0 - 0.5)
+l3.place(relheight=0.3, relwidth=1.0,rely=0.68)
+l4.place(relx=0, relheight=0.30, relwidth=0.5,rely=0.25)
+l5.place(relx=0, relheight=0.12, relwidth=0.5,rely=0.55)
+l6.place(relx=0.5, relheight=0.12, relwidth=0.5,rely=0.55)
+root.after(2000,verify)
+root.mainloop()
+if 'gestionnaire.db' in glob.glob("*.db"):
+    encrypt(password_temp)
